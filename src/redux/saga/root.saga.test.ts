793a1f3a917f6854af381fe1a9aafa3fdf26f381
Hemal Pandya <hemal@digiflux.io>
2019-08-20 12:10:25 +0530

    Added signup saga in root saga test

--- src/redux/saga/root.saga.test.ts
+++ src/redux/saga/root.saga.test.ts
@@ -2,10 +2,13 @@ import { rootSaga } from './root.saga';
 import { all } from 'redux-saga/effects';
 import { watchLogoutUserSaga } from './customer-logout/customer-logout.saga';
 import { watchLoginUserSaga } from './customer-login/customer-login.saga';
+import { watchSignUpUserSaga } from './customer-signup/customer-signup.saga';
 
 describe('root saga', () => {
     it('should react to the actions being called', () => {
         const generator = rootSaga();
-        expect(generator.next().value).toEqual(all([watchLoginUserSaga(), watchLogoutUserSaga()]));
+        expect(generator.next().value).toEqual(
+            all([watchLoginUserSaga(), watchLogoutUserSaga(), watchSignUpUserSaga()]),
+        );
     });
 });
