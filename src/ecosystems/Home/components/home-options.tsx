37eeca9a914ca5987c1c214562e465376edb35b2
Diego Romero <dev.diego.romero@gmail.com>
2019-09-16 22:16:43 +0100

    fixed size of images in landing

--- src/ecosystems/Home/components/home-options.tsx
+++ src/ecosystems/Home/components/home-options.tsx
@@ -31,7 +31,7 @@ const HomeOptions = () => (
         <CardDeck>
             {options.map((option, i) => (
                 <Card key={i} className={`mt-3 mb-5`} style={{ cursor: 'pointer' }}>
-                    <Card.Img variant="top" src={option.img} style={{ maxHeight: '270px' }} />
+                    <Card.Img variant="top" src={option.img} style={{ maxHeight: '222px' }} />
                     <Card.Body>
                         <Card.Title>{option.header}</Card.Title>
                         <Card.Text>{option.body}</Card.Text>
